630b7e28739896b02c487fcee93c644caf78ed7b
Usamali1 <usamaliaqat1264@gmail.com>
2021-07-20 20:31:23 +0500

    Fixes on desktop view

--- components/Home/Client.js
+++ components/Home/Client.js
@@ -10,17 +10,16 @@ const Client = () => {
             <div className="main_clients bg-gray-200 pb-72">
                 <h1 className="font-medium text-4xl font-montserrat mt-24 pt-16 mb-8 ml-44 text-primary ">HEAR FROM OUR CLIENTS</h1>
                 <p className="font-normal text-xs font-montserrat mt-4 mb-10 ml-44 text-secondary max-w-2xl">Neque, viverra nibh morbi in proin vel. Maecenas cursus sit eu ultrices amet elit. Amet scelerisque pellentesque vel tellus condimentum . </p>
-
-            </div>
+             </div>
             <div className="flex justify-around bg-primary mx-auto max-w-4xl absolute top-56 left-44">
 
-                <div className=" pt-20 pl-8 pr-6">
+                <div className=" my-6 pt-10 pl-8 pr-10 border-r border-black">
                     <img className="ml-8" src="images/Ellipse 3.png" alt="" />
                     <h1 className="font-medium text-2xl font-montserrat mt-6 mb-2 text-center text-secondary">Srishti Vashishtha</h1>
-                    <h1 className="font-normal text-base font-montserrat mb-10 text-center text-secondary">Real Estate Broker</h1>
+                    <h1 className="font-normal text-base italic font-montserrat mb-10 text-center text-secondary">Real Estate Broker</h1>
                 </div>
-                <div className=" bg-fadeprimary py-4 h-auto">
-                    <p className="font-normal text-xs font-montserrat mt-36 mb-10 px-2 text-secondary max-w-lg">Neque, viverra nibh morbi in proin vel. Maecenas cursus sit eu ultrices amet elit. Amet scelerisque pellentesque vel tellus condimentum malesuada sagittis faucibus pretium. Tellus, mauris eget urna purus, nam. Rhoncus in diam sapien fames at urna miNeque. Amet scelerisque entesque vel tellus condimentum malesuada sagittis faucibus pretium. Tellus, mauris eget urna purus, nam.</p>
+                <div >
+                    <p className="font-normal bg-fadeprimary text-xs font-montserrat mt-16 mx-10 mb-10 px-6 py-11 text-secondary max-w-lg">Neque, viverra nibh morbi in proin vel. Maecenas cursus sit eu ultrices amet elit. Amet scelerisque pellentesque vel tellus condimentum malesuada sagittis faucibus pretium. Tellus, mauris eget urna purus, nam. Rhoncus in diam sapien fames at urna miNeque. Amet scelerisque entesque vel tellus condimentum malesuada sagittis faucibus pretium. Tellus, mauris eget urna purus, nam.</p>
                 </div>
             </div>
         </div>
--- components/Home/Contact.js
+++ components/Home/Contact.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+            width: '52ch',
+        },
+    },
+}));
 const Contact = () => {
+    const classes = useStyles();
     return (
-        <div className=" relative">
-            <h1>Contact</h1>
-            
-        </div>
+        <div className="contact_main  flex justify-center mt-48">
+            <div className="bg-white my-20 py-10 mb-20 px-12 shadow-xl">
+                <div className="py-2.5 px-6 border-4 border-primary shadow-md">
+                    <h1 className="font-medium text-4xl font-montserrat mt-4 text-primary ">Contact</h1>
+
+                    <form className={classes.root} noValidate autoComplete="off">
+                        <TextField id="standard-basic" label="NAME" />
+                        <TextField id="standard-basic" label="PHONE NUMBER" />
+                        <br />
+                        <TextField id="standard-basic" label="MESSAGE" className='message-input'/>
+                    </form>
+                    <div className="flex justify-between">
+                        <img className="mt-8" src="images/logo.svg" alt="" />
+                        <div className="mt-20">
+                            <a className=" py-2 px-4 border-2 border-primary bg-primary text-white font-montserrat text-xl items-center hover:bg-white hover:text-primary transition-all" href="#">Send message</a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div >
     )
 }
 
